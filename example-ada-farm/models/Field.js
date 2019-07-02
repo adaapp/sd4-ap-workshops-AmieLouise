13a81e51da97c58c615818a2ce9a2391b8ab30bb
Amie Dooley <amiedooley1994@googlemail.com>
2019-07-02 15:27:15 +0100

    encapsulation of field model

--- example-ada-farm/models/Field.js
+++ example-ada-farm/models/Field.js
@@ -1,37 +1,68 @@
 class Field {
     constructor(name, health, x, y, width, height) {
-        this.name = name
-        this.health = health
-        this.x = x
-        this.y = y
-        this.width = width
-        this.height = height
-        this.crops = []
+        this._name = name
+        this._health = health
+        this._x = x
+        this._y = y
+        this._width = width
+        this._height = height
+        this._crops = []
     }
+
+    // GETTERS AND SETTERS //
+
+    getXPos(){
+        return this._x;
+    }
+    getYPos() {
+        return this._y;
+    }
+    getWidth() {
+        return this._width;
+    }
+    getHeight() {
+        return this._height;
+    }
+    getCrops(){
+        return this._crops;
+    }
+    getName(){
+        return this._name;
+    }
+
+    addCrops(cropType, field, x, y){
+        this._crops.push(new Crop(cropType, field, x, y))
+    }
+
+    
+    // OTHER FUNCTIONS // 
 
     draw() {
         stroke('#7c4011')
         strokeWeight(10)
         fill('#b58969')
-        rect(this.x, this.y, this.width, this.height, 10)
-        for (var crop of this.crops) {
+        const cropsArray = this.getCrops();
+        rect(this.getXPos(), this.getYPos(), this.getWidth(), this.getHeight(), 10)
+        for (var crop of cropsArray) {
             crop.draw()
         }
     }
 
+
     plant(x, y) {
         console.log('new plant')
         // Get the crop name from the field name
-        var cropName = this.name.split(' ') // => e.g. "Wheat"
+        var cropName = this.getName().split(' ') // => e.g. "Wheat"
         // Add a new crop to the field's list of crops
-        this.crops.push(new Crop(cropName[0], this, x, y))
+        this.addCrops(cropName[0], this, x, y)
+        // this.crops.push(new Crop(cropName[0], this, x, y))
     }
 
     containsPoint(x, y) {
-        var top = this.y
-        var bottom = this.y + this.height
-        var left = this.x
-        var right = this.x + this.width
+        var top = this.getYPos();
+        var bottom = this.getYPos() + this.getHeight();
+        var left = this.getXPos();
+        var right = this.getXPos() + this.getWidth();
 
         var isInVerticalRange = y > top && y < bottom
         var isInHorizontalRange = x > left && x < right
